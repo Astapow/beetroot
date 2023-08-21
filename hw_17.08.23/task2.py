6b4eef1f23332a75235959469c412cef5a4619b1
astapov <dmytroastapow@gmail.com>
2023-08-21 20:35:23 +0300

    hw 17/08

---
+++ hw_17.08.23/task2.py
@@ -0,0 +1,9 @@
+# let`s start
+print("let`s", "start", sep=',')
+
+# Python the best
+print("Python the best", end=' ! ')
+print()
+
+# using sep and end
+print("One", "Two", "Three", sep="-", end=' etc.')
