8b600835774eadb88835f42166315fecd3a5b765
astapov <dmytroastapow@gmail.com>
2023-10-11 18:44:34 +0300

    fix hw 05 Sep, Tue, 18:00-20:00

--- hw_05.09.23/task1/exception.py
+++ hw_05.09.23/task1/exception.py
@@ -10,10 +10,16 @@ def oops(first_num, second_num):
 # option 2
 
 try:
-    print(oops(2, "2"))
+    print(oops(2, 0))  # raise ZeroDivisionError
 except ZeroDivisionError:
     print("You can't divide by 0")
+
+try:
+    print(oops(2, "2"))  # raise TypeError
 except TypeError:
-    print("Enter the number ")
+    print("Pass numbers as parameters")
+
+try:
+    raise IndexError
 except IndexError:
     print("index mistake")
