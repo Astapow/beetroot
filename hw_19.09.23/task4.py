3f33bbb3ce7a3e05fbdbf454977ba7959e4c4bb3
astapov <dmytroastapow@gmail.com>
2023-10-14 21:23:22 +0300

    update hw 19 Sep, Tue, 18:00-20:00

--- hw_19.09.23/task4.py
+++ hw_19.09.23/task4.py
@@ -1,14 +1,17 @@
 class CustomException(Exception):
     def __init__(self, msg):
-        super().__init__(msg)
+        self.msg = msg
         self.create_log(msg)
 
     def create_log(self, msg):
-        with open('logs.txt', 'w') as file:
+        with open('logs.txt', 'a') as file:
             file.write(msg + '\n')
+
+    def __str__(self):
+        return f"CustomException {self.msg}"
 
 
 try:
-    raise CustomException("some error")
+    raise CustomException("second err")
 except CustomException as exc:
     print(exc)
